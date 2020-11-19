1634c0eb052088dc56e2d0d54af4cf16af881151
BroJoon <ydngjink1@naver.com>
2020-11-20 06:32:40 +0900

    1009

---
+++ BOJ/1009.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int t, a, b;
+  int result, temp;
+  scanf("%d", &t);
+  while(t--) {
+    scanf("%d %d", &a, &b);
+    temp = a;
+    --b;
+    while(b--) {
+      a = (a * temp) % 10;
+      if (a == 0)
+        a = 10;
+    }
+    result = a % 10;
+    if (!result) {
+      printf("10\n");
+    } else {
+      printf("%d\n", result);
+    }
+  }
+  return 0;
+}
