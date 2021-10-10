e91f18c0121542ebdf02a118d6b2861f91e1a85f
BroJoon <ydngjink1234@gmail.com>
2021-10-10 23:16:48 +0900

    change: 오픈채팅방

--- Implementation/Programers_오픈채팅방.js
+++ Implementation/Programers_오픈채팅방.js
@@ -1,36 +1,29 @@
 function solution(record) {
-	let arr = [];
-	var answer = [];
-	for (int i = 0; i < record.size(); i++)
-			arr.push(record.split(" "));
-	set = new Set();
-	for (int i = record.size() - 1; i >= 0; i--)
-	{
-			if (!(set.has(arr[i][1])))
-			{
-					if (arr[i][0] != "Leave")
-					{   
-							set.add(arr[i][1]);
-							map.set(arr[i][1], arr[i][2]);
-					}
-			}
-	}
-	for (int i = 0; i < record.size(); i++)
-	{
-			let result = "";
-			if (arr[i][0] == "Enter")
-			 {
-					result += map.get(arr[i][1]);
-					result += "님이 들어왔습니다.";
-					answer.push(result);
-			 }   
-			else if(arr[i][0] == "Leave")
-			{
-					result += map.get(arr[i][1]);
-					result += "님이 나갔습니다.";
-					answer.push(result);
-			}
-	}
-	
-	return answer;
+  let arr = [];
+  var answer = [];
+  for (let i = 0; i < record.length; i++) arr.push(record[i].split(" "));
+  let userId = new Set();
+  let userNick = new Map();
+  for (let i = record.length - 1; i >= 0; i--) {
+    if (!userId.has(arr[i][1])) {
+      if (arr[i][0] != "Leave") {
+        userId.add(arr[i][1]);
+        userNick.set(arr[i][1], arr[i][2]);
+      }
+    }
+  }
+  for (let i = 0; i < record.length; i++) {
+    let result = "";
+    if (arr[i][0] == "Enter") {
+      result += userNick.get(arr[i][1]);
+      result += "님이 들어왔습니다.";
+      answer.push(result);
+    } else if (arr[i][0] == "Leave") {
+      result += userNick.get(arr[i][1]);
+      result += "님이 나갔습니다.";
+      answer.push(result);
+    }
+  }
+
+  return answer;
 }
