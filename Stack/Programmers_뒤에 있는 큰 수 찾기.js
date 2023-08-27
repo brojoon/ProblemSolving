f75a0373e200ecb84ba9cd2a0dd550a07e057681
brojoon <ydngjink1234@gmail.com>
2023-08-27 15:39:25 +0900

    Stack 이용해서 O(n)으로 시간초과 해결

---
+++ Stack/Programmers_뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const answer = new Array(numbers.length).fill(-1);
+    const stack = [];
+  
+    for(let i = 0; i < numbers.length; i++){
+      while(stack.length > 0 && numbers[stack[stack.length-1]] < numbers[i]){
+        answer[stack.pop()] = numbers[i]; 
+      }
+      stack.push(i) 
+    }
+      
+    return answer;
+  }
