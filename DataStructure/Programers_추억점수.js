83454f3e9e7af2655e2cb1afec6202c88b2fe1c5
brojoon <ydngjink1234@gmail.com>
2023-09-19 22:49:29 +0900

    obj에 key, value로 저장

---
+++ DataStructure/Programers_추억점수.js
@@ -0,0 +1,10 @@
+function solution(name, yearning, photo) {
+    const memory = {};
+    for (let i = 0; i < name.length; i++) {
+      memory[name[i]] = yearning[i];
+    }
+  
+    return photo.map((people) =>
+      people.reduce((acc, cur) => acc + (memory[cur] || 0), 0)
+    );
+  }
